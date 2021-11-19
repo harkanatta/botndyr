a344fd1c3e53abaf9b65239c0c8856da8216ba70
harkanatta <valtyrgullhjarta@gmail.com>
2021-11-19 12:40:10 +0000

    nenertea og mollusca

--- skjol/species-identification/loop.R
+++ skjol/species-identification/loop.R
@@ -3,16 +3,17 @@
 library(httr)
 library(rvest)
 library(tibble)
-library(xml2)
+#library(xml2)
 
 URL <- "http://species-identification.org"
 flokkun <- list()
 # Fyrsti ormurinn í Macrobenthos of the North Sea - Polychaeta M.J. de Kluijver et al.:
  Ormur <- "species.php?species_group=macrobenthos_polychaeta&menuentry=soorten&id=402&tab=beschrijving"
-#Skel: Ormur <- "species.php?species_group=mollusca&menuentry=soorten&id=513&tab=beschrijving"
-
+#Skel: Ormur <- "species.php?species_group=mollusca&menuentry=soorten&id=513&tab=beschrijving" #i=506
+#Ranaormar Ormur <- "species.php?species_group=macrobenthos_nemertina&id=39"
+ 
 # þessi for-lúppa þarf að vera mötuð af hlekknum 'Ormur' til þess að byrja á skrefi 1 þar sem hún klippir og límir töflu af vefsíðunni í listann 'flokkun'. Í skrefi 2 sækir hún hlekkinnn á næsta orm fyrir skref 1 og svo koll af kolli þar til einhver leiðinda "encoding"-villa kemur upp en þá þarf að finna síðasta orminn handvirkt og reyna næsta  og næsta orm þar til lúppan kemst af stað aftur. Það er ekkert stopp á lúppunni en ormarnir eru um 500.
-for (i in 1:1000) {
+for (i in 1:200) {
   
   #1
   POST(paste(URL,Ormur,sep = "/")) %>%
@@ -32,7 +33,7 @@ for (i in 1:1000) {
 
 rass <- do.call(rbind, flokkun)
 rass <- rass[!duplicated(rass),]
-write.csv(rass,"Polychaeta.csv")
+#write.csv(rass,"Polychaeta.csv")
 
 
 ###<>-<>-<>
@@ -40,10 +41,19 @@ write.csv(rass,"Polychaeta.csv")
 ###<>-<>-<>
 ###<>-<>-<>
 ###<>-<>-<>
+
+
+# url <- "https://en.wikipedia.org/wiki/Taxonomic_rank"
+# webpage <- read_html(url)
+# tbls <- html_nodes(webpage, "table")
+# df <- html_table(tbls[grep("Infratribe",tbls,ignore.case = T)],fill = T)[[1]]
+# df$Rank <- gsub("([A-Za-z]+).*", "\\1", df$Rank)
 
 
 
 ormar <- read.csv("skjol/species-identification/Polychaeta.csv")
+ormar <- read.csv("skjol/species-identification/mollusca.csv")
+ormar <- read.csv("skjol/species-identification/nemertea.csv")
 
 rass <- list()
 DF <- data.frame()
@@ -56,7 +66,13 @@ for (z in 1:dim(ormar)[1]) {
   rass[z] <- list(DF)
 }
 
+DF <- data.frame(matrix(ncol = 13, nrow = 0))
+dalkar <- c("Kingdom","Phylum","Subphylum","Class", "Subclass", "Superorder","Order","Suborder","Superfamily","Family","Subfamily","Genus","Species")
+colnames(DF) <- dalkar
+rass[z+1] <- list(DF)
+
 rass <- rass[order(sapply(rass,ncol),decreasing = T)]
+rass[[1]] <- rass[[1]] %>% mutate(across(where(is.logical), as.character))
 
 Polychaeta <- do.call(dplyr::bind_rows, rass)
 DT::datatable(Polychaeta[,-c(1,2,3)])
